11a5962aacd6a169f98920c9eddf104a92fde0f0
liujianhui <yukioy07@163.com>
2023-11-21 14:02:20 +0800

    更新sql文件

--- sql/sys_file_record.sql
+++ sql/sys_file_record.sql
@@ -1,4 +1,18 @@
+/*
+ Navicat Premium Data Transfer
 
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 80034
+ Source Host           : localhost:3306
+ Source Schema         : tyuexam
+
+ Target Server Type    : MySQL
+ Target Server Version : 80034
+ File Encoding         : 65001
+
+ Date: 21/11/2023 13:32:27
+*/
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
@@ -7,26 +21,27 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for sys_file_record
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_file_record`;
-CREATE TABLE `sys_file_record`  (
-  `id` bigint(32) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `file_urls` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '上传分片的链接',
-  `file_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '文件名',
-  `md5` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'MD5',
-  `upload_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '上传id',
-  `is_uploaded` tinyint(1) NULL DEFAULT 0 COMMENT '是否已上传',
-  `total_chunks` int(9) NULL DEFAULT NULL COMMENT '分片总块数',
-  `size` bigint(20) NULL DEFAULT NULL COMMENT '文件大小（B）',
-  `completed_parts` int(6) NULL DEFAULT 0 COMMENT '已完成片数',
-  `create_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '创建者',
-  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
-  `update_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '更新者',
-  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
-  `remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '备注',
-  `file_url` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '文件地址',
-  `bucket` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'minio桶名称',
-  `chunk_result` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '各分片上传的结果',
-  `version` bigint(20) NULL DEFAULT 0 COMMENT '版本',
+CREATE TABLE `sys_file_record` (
+  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `file_urls` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT '上传分片的链接',
+  `file_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '文件名',
+  `md5` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'MD5',
+  `upload_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '上传id',
+  `is_uploaded` tinyint(1) DEFAULT '0' COMMENT '是否已上传',
+  `total_chunks` int DEFAULT NULL COMMENT '分片总块数',
+  `size` bigint DEFAULT NULL COMMENT '文件大小（B）',
+  `completed_parts` int DEFAULT '0' COMMENT '已完成片数',
+  `create_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '' COMMENT '创建者',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '' COMMENT '更新者',
+  `update_time` datetime DEFAULT NULL COMMENT '更新时间',
+  `remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '备注',
+  `file_url` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '文件地址',
+  `bucket` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT 'minio桶名称',
+  `chunk_result` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT '各分片上传的结果',
+  `version` bigint DEFAULT '0' COMMENT '版本',
+  `is_recycle` int DEFAULT '0' COMMENT '0在回收站 1 不在',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 66 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '文件上传记录表' ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=117 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC COMMENT='文件上传记录表';
 
 SET FOREIGN_KEY_CHECKS = 1;
